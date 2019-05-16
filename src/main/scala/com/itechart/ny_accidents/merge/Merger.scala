376b64696244330d1cc25468cfd4a4fef92ea439
Alexandra <nistyuk@protonmail.com>
2019-05-16 19:43:29 +0300

    add merge accidents and weather

--- src/main/scala/com/itechart/ny_accidents/merge/Merger.scala
+++ src/main/scala/com/itechart/ny_accidents/merge/Merger.scala
@@ -1,9 +1,12 @@
 package com.itechart.ny_accidents.merge
 
+import java.time.format.DateTimeFormatter
+
 import com.itechart.ny_accidents.districts.controller.DistrictsDatabase
 import com.itechart.ny_accidents.districts.database.DistrictsDao
 import com.itechart.ny_accidents.entity.{AccidentsNY, MergedData}
-import com.itechart.ny_accidents.utils.PostgisUtils
+import com.itechart.ny_accidents.utils.{DateUtils, PostgisUtils}
+import com.itechart.ny_accidents.weather.WeatherMappingService
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
@@ -23,6 +26,8 @@ object Merger {
     val dao = new DistrictsDao
     val fut = DistrictsDatabase.database.run(dao.getByCoordinates(PostgisUtils.createPoint(value.latitude.getOrElse(0), value.longitude.getOrElse(0))))
     val district = Await.result(fut, Duration.Inf)
-    MergedData(value, district)
+    val service = new WeatherMappingService()
+    val weather = service.findWeatherByTimeAndCoordinates(DateUtils.parseDate(value.date.toString ,DateTimeFormatter.ofPattern("MM/d/yyyy")).getOrElse(0), value.latitude.getOrElse(0), value.longitude.getOrElse(0))
+    MergedData(value, district, weather)
   }
 }
