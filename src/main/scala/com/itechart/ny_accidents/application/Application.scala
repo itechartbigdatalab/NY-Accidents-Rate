e33f9bf5777080137bf108f84649f452b1cc2162
AlexKostritsa <askoalexandra@gmail.com>
2019-05-17 22:55:13 +0300

    working master

--- src/main/scala/com/itechart/ny_accidents/application/Application.scala
+++ src/main/scala/com/itechart/ny_accidents/application/Application.scala
@@ -1,9 +1,11 @@
 package com.itechart.ny_accidents.application
 
+import com.itechart.ny_accidents.entity.MergedData
 import com.itechart.ny_accidents.merge.Merger
 import com.itechart.ny_accidents.spark.Spark
 import com.itechart.ny_accidents.utils.CsvReader
 import com.typesafe.config.ConfigFactory
+import org.apache.spark.rdd.RDD
 
 
 object Application extends App {
@@ -11,10 +13,20 @@ object Application extends App {
   val filesConfig = ConfigFactory.load("app.conf")
   val pathToDataFolder = filesConfig.getString("file.inputPath")
   val inputFileAccidents = pathToDataFolder + filesConfig.getString("file.input.inputFileNYAccidents")
-  val raws = Spark.sc.parallelize(CsvReader.readData(inputFileAccidents)).cache()
-  val mergedData = Merger(raws)
-  //  val q = mergedData.take(8)
+
+  val raws = CsvReader.readData(inputFileAccidents)
+  val sc = Spark.sc
+  val mergedData = sc.parallelize(Merger(raws))
+  val q = mergedData.take(8)
+  println(mergedData.count())
+  println(raws.size)
+  println(q)
+
+  println(raws.size + "KISAKISA")
+
+  //  val raws = Spark.sc.parallelize(CsvReader.readData(inputFileAccidents))
+  //  val mergedData: RDD[MergedData] = Merger(raws)
+  //  //  val q = mergedData.take(8)
   //  println(mergedData.count())
-  println(raws.count() + "KISAKISA")
   //  println(q)
 }
--- src/main/scala/com/itechart/ny_accidents/merge/Merger.scala
+++ src/main/scala/com/itechart/ny_accidents/merge/Merger.scala
@@ -11,14 +11,14 @@ object Merger {
   private val ds = new DistrictsService
   private val service = new WeatherMappingService()
 
-  def apply(accidents: RDD[Accident]): RDD[MergedData] = {
+  def apply(accidents: Seq[Accident]):  Seq[MergedData] ={
     accidents.map(mapper)
   }
 
-  private def mapper(value: Accident): MergedData = {
+  private def mapper(value: Accident): MergedData ={
 
-    if (value.longitude.isDefined && value.latitude.isDefined) {
-      val district = ds.getDistrict(value.latitude.getOrElse(0), value.longitude.getOrElse(0), DistrictsStorage.data)
+    if (value.longitude.isDefined && value.latitude.isDefined ) {
+      val district = ds.getDistrict(value.latitude.getOrElse(0), value.longitude.getOrElse(0), DistrictsStorage.data )
       if (value.dateTimeMillis.isDefined) {
         val weather = service.findWeatherByTimeAndCoordinates(value.dateTimeMillis.getOrElse(0), value.latitude.getOrElse(0), value.longitude.getOrElse(0))
         MergedData(value, district, weather)
