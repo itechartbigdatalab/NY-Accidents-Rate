aed70db56ccf9d7a0d364e7e304354b67f2f40eb
AlexKostritsa <askoalexandra@gmail.com>
2019-05-20 11:05:46 +0300

    empty App

--- src/main/scala/com/itechart/ny_accidents/Application.scala
+++ src/main/scala/com/itechart/ny_accidents/Application.scala
@@ -7,24 +7,5 @@ import com.typesafe.config.ConfigFactory
 
 object Application extends App {
 
-  val filesConfig = ConfigFactory.load("app.conf")
-  val pathToDataFolder = filesConfig.getString("file.inputPath")
-  val inputFileAccidents = pathToDataFolder + filesConfig.getString("file.input.inputFileNYAccidents")
-  val accidentsParser = new AccidentsParser
 
-  val raws = accidentsParser.readData(inputFileAccidents)
-  val mergedData = MergeService.mergeAccidentsWithWeatherAndDistricts(raws).cache()
-
-  val dayOfWeek = WeatherMetric.countDayOfWeek(mergedData)
-  val hourOfDay = WeatherMetric.countHours(mergedData)
-  val lightPeriods = WeatherMetric.definePeriod(mergedData)
-
-  println(mergedData.count)
-  println()
-  dayOfWeek.foreach(println)
-  println()
-  hourOfDay.foreach(println)
-  println()
-  lightPeriods.foreach(println)
-  println()
 }
