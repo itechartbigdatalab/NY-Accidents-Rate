404e682c83ac78bc72c4cb8ce5ab4446fc6df0b0
baklanvi <baklanvi@gmail.com>
2019-05-22 12:57:38 +0300

    Created Configuration object and all configs were migrated to resources/conf

--- src/main/scala/com/itechart/ny_accidents/constants/Configuration.scala
+++ src/main/scala/com/itechart/ny_accidents/constants/Configuration.scala
@@ -3,13 +3,31 @@ package com.itechart.ny_accidents.constants
 import com.typesafe.config.ConfigFactory
 
 object Configuration {
-  private val appConf = ConfigFactory.load("app.conf")
-  private val cacheConf = ConfigFactory.load("conf/cache.conf")
+  private lazy val appConf = ConfigFactory.load("conf/app.conf")
+  private lazy val cacheConf = ConfigFactory.load("conf/cache.conf")
+  private lazy val databaseConf = ConfigFactory.load("conf/ny_data_database.conf")
 
-  val CACHE_NAME: String = cacheConf.getString("cache.name")
-  val CACHE_PATH: String = "/home/" +
+  lazy val CACHE_NAME: String = cacheConf.getString("cache.name")
+  lazy val CACHE_PATH: String = "/home/" +
     System.getProperty("user.name") + "/" + CACHE_NAME
-  val CACHE_DISK_SIZE: Int = cacheConf.getInt("cache.disk_size_gb")
-  val CACHE_HEAP_SIZE: Int = cacheConf.getInt("cache.heap_size_units")
-  val CACHE_OFF_HEAP_SIZE: Int = cacheConf.getInt("cache.off_head_size_gb")
+  lazy val CACHE_DISK_SIZE: Int = cacheConf.getInt("cache.disk_size_gb")
+  lazy val CACHE_HEAP_SIZE: Int = cacheConf.getInt("cache.heap_size_units")
+  lazy val CACHE_OFF_HEAP_SIZE: Int = cacheConf.getInt("cache.off_head_size_gb")
+  lazy val DATA_FILE_PATH: String = appConf.getString("file.inputPath") +
+    appConf.getString("file.input.inputFileNYAccidents")
+
+  lazy val NY_DATA_DATABASE_URL: String = databaseConf.getString("postgreConf.url")
+  lazy val NY_DATA_DATABASE_DRIVER: String = databaseConf.getString("postgreConf.driver")
+  lazy val NY_DATA_DATABASE_USER: String = databaseConf.getString("postgreConf.user")
+  lazy val NY_DATA_DATABASE_PASSWORD: String = databaseConf.getString("postgreConf.password")
+  lazy val NY_DATA_DATABASE_CONNECTION_POOL: String = databaseConf.getString("postgreConf.connectionPool")
+  lazy val NY_DATA_DATABASE_KEEP_ALIVE_CONNECTION: Boolean = databaseConf.getBoolean("postgreConf.keepAliveConnection")
+
+  lazy val NYNTA_PATH: String = appConf.getString("file.nynta_path")
+
+  lazy val REDIS_HOST: String = "127.0.0.1"
+  lazy val REDIS_PORT: Int = 6379
+  lazy val REDIS_POOL_SIZE: Int = 128
+
+  lazy val MONGO_HOST: String = "mongodb://localhost:27017"
 }
