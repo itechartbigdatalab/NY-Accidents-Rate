06abae29dce7def11ef0b84300ac4ff81c6f1252
baklanvi <baklanvi@gmail.com>
2019-05-22 13:00:42 +0300

    Refactored NyDataDatabaseMongo (Replaced constant by Config value)

--- src/main/scala/com/itechart/ny_accidents/database/NyDataDatabaseMongo.scala
+++ src/main/scala/com/itechart/ny_accidents/database/NyDataDatabaseMongo.scala
@@ -1,13 +1,14 @@
 package com.itechart.ny_accidents.database
 
+import com.itechart.ny_accidents.constants.Configuration
 import org.mongodb.scala.{MongoClient, MongoDatabase}
 
 @Deprecated
 object NyDataDatabaseMongo {
-  private final val MONGO_HOST = "mongodb://localhost:27017"
   private final val DATABASE = "ny_data"
 
-  private lazy val client: MongoClient = MongoClient(MONGO_HOST + "/?waitQueueMultiple=100")
+  private lazy val client: MongoClient = MongoClient(Configuration.MONGO_HOST
+    + "/?waitQueueMultiple=100")
 
   lazy val database: MongoDatabase = client.getDatabase(DATABASE)
 }
