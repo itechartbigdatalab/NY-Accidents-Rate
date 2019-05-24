cd8f6f0e30cb49441a0a88776446f41df6f45f9b
baklanvi <baklanvi@gmail.com>
2019-05-24 11:18:47 +0300

    #NEAF-64 Added log4j properties to disable spark logging

--- src/main/scala/com/itechart/ny_accidents/spark/Spark.scala
+++ src/main/scala/com/itechart/ny_accidents/spark/Spark.scala
@@ -1,10 +1,15 @@
 package com.itechart.ny_accidents.spark
 
-import org.apache.log4j.{Level, Logger}
+import org.apache.log4j.{Level, LogManager, Logger}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Spark {
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
+  Logger.getLogger("spark").setLevel(Level.OFF)
+  LogManager.getRootLogger.setLevel(Level.OFF)
+
   val conf: SparkConf = new SparkConf().setAppName("ny_spark").setMaster("local[*]")
   val sc = new SparkContext(conf)
   val sparkSql: SparkSession = SparkSession.builder
