cfc8d7cdbdb634e0407cd8ba899876e2112f5121
AlexKostritsa <askoalexandra@gmail.com>
2019-05-24 14:02:28 +0300

    fixed bug with PopulationStorage rwriting population metric count with classes

--- src/main/scala/com/itechart/ny_accidents/service/metric/PopulationMetricService.scala
+++ src/main/scala/com/itechart/ny_accidents/service/metric/PopulationMetricService.scala
@@ -4,17 +4,18 @@ import com.google.inject.Inject
 import com.itechart.ny_accidents.database.dao.PopulationStorage
 import com.itechart.ny_accidents.entity.MergedData
 import com.itechart.ny_accidents.service.PopulationService
-import com.itechart.ny_accidents.spark.Spark
+import javax.inject.Singleton
 import org.apache.spark.rdd.RDD
 
-object PopulationMetricService {
+@Singleton
+class PopulationMetricService @Inject()(populationStorage: PopulationStorage) {
 
   def getPopulationToNumberOfAccidentsRatio(data: RDD[MergedData]): RDD[(String, Double)] = {
-
+    val populationMap = populationStorage.populationMap
     data.filter(_.district.isDefined).groupBy(_.district.get).map{case (district, mergedData) => {
       val districtHash = district.hashCode()
       val accidentsNumber = mergedData.size
-      PopulationStorage.populationMap.get(districtHash) match {
+      populationMap.get(districtHash) match {
         case Some(value) =>
           val ration = accidentsNumber / PopulationService.calculateDensity(value)
           Some(district.districtName, ration)
