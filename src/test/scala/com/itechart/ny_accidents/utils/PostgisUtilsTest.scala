50f8ea9c6bbfed68a203e853a698204487ceead3
Vladimir Baklan <34661735+VladimirBaklan@users.noreply.github.com>
2019-05-27 14:13:13 +0300

    #NEAF-19 unit tests (#26)
    * #NEAF-19 add tests
    
    * #NEAF-19 fix unit tests edits

---
+++ src/test/scala/com/itechart/ny_accidents/utils/PostgisUtilsTest.scala
@@ -0,0 +1,17 @@
+package com.itechart.ny_accidents.utils
+
+import com.vividsolutions.jts.geom.{Coordinate, GeometryFactory}
+import org.scalatest.FunSpec
+
+class PostgisUtilsTest extends FunSpec {
+  describe("A PostgisUtils") {
+    it("should create postgis Point in format like (longitude, latitude)") {
+      val (testDataLatitude, testDataLongitude) = (-70.0, 40.0)
+      val geometryFactory = new GeometryFactory
+      val expectedValue = geometryFactory.createPoint(new Coordinate(testDataLongitude, testDataLatitude))
+      val result = PostgisUtils.createPoint(testDataLatitude, testDataLongitude)
+
+      assert(result == expectedValue)
+    }
+  }
+}
