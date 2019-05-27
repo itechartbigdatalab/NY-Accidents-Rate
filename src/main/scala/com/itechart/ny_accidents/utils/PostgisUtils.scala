5a0a74efc73bcb957794ad9f385d403ebbe20923
VladimirBaklan <16it1.baklan.u@gmail.com>
2019-05-27 10:03:01 -0400

    #NEAF-71 add SRID support in postgis utils. Update PostgisUtils tests

--- src/main/scala/com/itechart/ny_accidents/utils/PostgisUtils.scala
+++ src/main/scala/com/itechart/ny_accidents/utils/PostgisUtils.scala
@@ -1,16 +1,18 @@
 package com.itechart.ny_accidents.utils
 
 import com.vividsolutions.jts.geom
-import com.vividsolutions.jts.geom.{Coordinate, Geometry, Point}
+import com.vividsolutions.jts.geom.{Coordinate, Geometry, Point, PrecisionModel}
 import com.vividsolutions.jts.io.WKTReader
 
 object PostgisUtils {
+  private lazy val SRID = 2163
+
   def getGeometryFromText(text: String): Geometry = {
     new WKTReader().read(text)
   }
 
   def createPoint(latitude: Double, longitude: Double): Point = {
-    val geometryFactory = new geom.GeometryFactory
+    val geometryFactory = new geom.GeometryFactory(new PrecisionModel(), SRID)
     // PostgreDocumentation -> https://postgis.net/docs/ST_Point.html
     // geometry ST_Point(float x_lon, float y_lat);
     geometryFactory.createPoint(new Coordinate(longitude, latitude))
