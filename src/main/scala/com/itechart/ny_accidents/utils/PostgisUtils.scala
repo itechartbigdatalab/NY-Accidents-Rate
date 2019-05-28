2ea47a753e6b4cb4d7a400aea7ae1489cde634a9
baklanvi <baklanvi@gmail.com>
2019-05-28 11:19:16 +0300

    #NEAF-71 rename SRID constant

--- src/main/scala/com/itechart/ny_accidents/utils/PostgisUtils.scala
+++ src/main/scala/com/itechart/ny_accidents/utils/PostgisUtils.scala
@@ -5,14 +5,14 @@ import com.vividsolutions.jts.geom.{Coordinate, Geometry, Point, PrecisionModel}
 import com.vividsolutions.jts.io.WKTReader
 
 object PostgisUtils {
-  private lazy val SRID = 2163
+  private lazy val US_NATIONAL_ALTLAS_AREA_SRID = 2163
 
   def getGeometryFromText(text: String): Geometry = {
     new WKTReader().read(text)
   }
 
   def createPoint(latitude: Double, longitude: Double): Point = {
-    val geometryFactory = new geom.GeometryFactory(new PrecisionModel(), SRID)
+    val geometryFactory = new geom.GeometryFactory(new PrecisionModel(), US_NATIONAL_ALTLAS_AREA_SRID)
     // PostgreDocumentation -> https://postgis.net/docs/ST_Point.html
     // geometry ST_Point(float x_lon, float y_lat);
     geometryFactory.createPoint(new Coordinate(longitude, latitude))
