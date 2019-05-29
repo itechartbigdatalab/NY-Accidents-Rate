00cf3f232249fa4811c0e3eeac51e044d2e1aebf
baklanvi <baklanvi@gmail.com>
2019-05-29 13:48:06 +0300

    #NEAF-75 add pattern matching DatasetSplitApplication

--- src/main/scala/com/itechart/ny_accidents/DatasetSplitApplication.scala
+++ src/main/scala/com/itechart/ny_accidents/DatasetSplitApplication.scala
@@ -4,16 +4,17 @@ import com.google.inject.Guice
 import com.itechart.ny_accidents.constants.Configuration
 import com.itechart.ny_accidents.parse.AccidentsParser
 import com.itechart.ny_accidents.utils.FileWriterUtils
+import com.itechart.ny_accidents.constants.Configuration._
 
 object DatasetSplitApplication extends App {
   val injector = Guice.createInjector(new GuiceModule)
   val accidentsParser = injector.getInstance(classOf[AccidentsParser])
 
-  val (schema, datasets) = accidentsParser.splitDatasetByYear(Configuration.PATH_TO_DATASET_TO_SPLIT)
-  datasets.foreach{case (key, value) =>
-    FileWriterUtils.createNewCsv(
-      s"${Configuration.SPLIT_DATASET_BASE_FOLDER}/${Configuration.CSV_BASE_NAME}_$key.csv",
-      value,
-      schema)}
-
+  accidentsParser.splitDatasetByYear(PATH_TO_DATASET_TO_SPLIT) match {case (schema, datasets) => {
+    datasets.foreach{case (key, value) =>
+      FileWriterUtils.createNewCsv(
+      s"$SPLIT_DATASET_BASE_FOLDER/${CSV_BASE_NAME}_$key.csv",
+      value, schema)
+    }
+  }}
 }
