55fa14bfc24ded785a0cc329b8435464a1b28283
baklanvi <baklanvi@gmail.com>
2019-05-29 15:30:08 +0300

    #NEAF-87 generate reports into database using report generators

--- src/main/scala/com/itechart/ny_accidents/Application.scala
+++ src/main/scala/com/itechart/ny_accidents/Application.scala
@@ -3,19 +3,18 @@ package com.itechart.ny_accidents
 
 import com.google.inject.Guice
 import com.itechart.ny_accidents.constants.Configuration
-import com.itechart.ny_accidents.constants.GeneralConstants._
+import com.itechart.ny_accidents.database.NYDataDatabase
 import com.itechart.ny_accidents.database.dao.PopulationStorage
 import com.itechart.ny_accidents.database.dao.cache.MergedDataCacheDAO
 import com.itechart.ny_accidents.entity.{Accident, MergedData}
 import com.itechart.ny_accidents.parse.AccidentsParser
 import com.itechart.ny_accidents.report.Reports
+import com.itechart.ny_accidents.report.generators._
 import com.itechart.ny_accidents.service.MergeService
-import com.itechart.ny_accidents.service.metric.{DistrictMetricService, PopulationMetricService, TimeMetricService, WeatherMetricService}
-import com.itechart.ny_accidents.utils.FileWriterUtils
+import com.itechart.ny_accidents.service.metric.{PopulationMetricService, WeatherMetricService}
+import com.itechart.ny_accidents.constants.ReportsDatabase._
 import org.apache.spark.rdd.RDD
 import org.slf4j.LoggerFactory
-import com.itechart.ny_accidents.constants.ReportsDatabase._
-import com.itechart.ny_accidents.database.NYDataDatabase
 
 object Application extends App {
   lazy val logger = LoggerFactory.getLogger(getClass)
@@ -36,61 +35,27 @@ object Application extends App {
     .mergeAccidentsWithWeatherAndDistricts[Accident, MergedData](raws, mergeService.mapper).cache()
   logger.info("Merged data size: " + mergeData.count())
 
-  val dayOfWeek: RDD[(String, Int, Double)] = TimeMetricService.countDayOfWeek(mergeData)
-  logger.info("Day of week calculated")
-  val hourOfDay: RDD[(Int, Int, Double)] = TimeMetricService.countHours(mergeData)
-  logger.info("Hour of day calculated")
-  val period: RDD[(String, Int, Double)] = weatherMetricService.definePeriod(mergeData)
-  logger.info("Period calculated")
-  val weatherPhenomenon: RDD[(String, Int, Double)] = weatherMetricService.getPhenomenonPercentage(mergeData)
-  logger.info("Weather phenomenon calculated")
-  val boroughPercentage: RDD[(String, Int, Double)] = DistrictMetricService.getBoroughPercentage(mergeData)
-  logger.info("Borough percentage calculated")
-  val districtsPercentage: RDD[(String, Int, Double)] = DistrictMetricService.getDistrictsPercentage(mergeData)
-  logger.info("Districts percentage calculated")
-  val populationToNumberOfAccidents: RDD[(String, Double, Double, Int)] = populationService
-    .getPopulationToNumberOfAccidentsRatio(mergeData)
-  logger.info("Population to number of accidents calculated")
-  val accidentCountDuringPhenomenonPerHour: RDD[(String, Int, Double, Double)] =
-    weatherMetricService.calculateAccidentCountDuringPhenomenonPerHour(mergeData)
-  logger.info("Accidents count per hour for each phenomenon metric calculated")
+  val creationDate = org.apache.spark.sql.functions.current_date()
+  val reports = injector.getInstance(classOf[Reports])
 
+  val dayOfWeekReport = new DayOfWeekReportGenerator().apply(mergeData, reports, creationDate)
+  val hourOfDayReport = new HourOfDayReportGenerator().apply(mergeData, reports, creationDate)
+  val periodReport = new PeriodReportGenerator().apply(mergeData, reports, creationDate)
+  val weatherReport = new WeatherReportGenerator().apply(mergeData, reports, creationDate)
+  val boroughReport = new BoroughReportGenerator().apply(mergeData, reports, creationDate)
+  val districtReport = new DistrictReportGenerator().apply(mergeData, reports, creationDate)
+  val populationToNumberOfAccidentsReport = new PopulationToNumberOfAccidentsReportGenerator(populationService)
+    .apply(mergeData, reports, creationDate)
+  val accidentCountDuringPhenomenonPerHourReport = new AccidentCountDuringPhenomenonPerHourReportGenerator()
+    .apply(mergeData, reports, creationDate)
 
-  val reportGenerateDate = org.apache.spark.sql.functions.current_date()
-
-  val report = injector.getInstance(classOf[Reports])
-  val dayOfWeekReport = report.generateDataFrameReportForTupleRDD(dayOfWeek, DAY_OF_WEEK_REPORT_SCHEMA, reportGenerateDate)
   NYDataDatabase.insertDataFrame(DAY_OF_WEEK_REPORT_TABLE_NAME, dayOfWeekReport)
-  logger.info("Day of week report created")
-
-  val hourOfDayReport = report.generateDataFrameReportForTupleRDD(hourOfDay, HOUR_OF_DAY_REPORT_SCHEMA, reportGenerateDate)
   NYDataDatabase.insertDataFrame(HOUR_OF_DAY_REPORT_TABLE_NAME, hourOfDayReport)
-  logger.info("Hour of day report created")
-
-  val periodReport = report.generateDataFrameReportForTupleRDD(period, DAY_OF_WEEK_REPORT_SCHEMA, reportGenerateDate)
   NYDataDatabase.insertDataFrame(DAY_PERIOD_REPORT_TABLE_NAME, periodReport)
-  logger.info("Period report created")
-
-  val weatherReport = report.generateDataFrameReportForTupleRDD(weatherPhenomenon, PHENOMENON_REPORT_SCHEMA, reportGenerateDate)
   NYDataDatabase.insertDataFrame(PHENOMENON_REPORT_TABLE_NAME, weatherReport)
-  logger.info("Weather report created")
-
-  val boroughReport = report.generateDataFrameReportForTupleRDD(boroughPercentage, BOROUGH_REPORT_SCHEMA, reportGenerateDate)
   NYDataDatabase.insertDataFrame(BOROUGH_REPORT_TABLE_NAME, boroughReport)
-  logger.info("Borough report created")
-
-  val districtsReport = report.generateDataFrameReportForTupleRDD(districtsPercentage, DISTRICT_REPORT_SCHEMA, reportGenerateDate)
-  println("REPORT: " + districtsReport)
-  NYDataDatabase.insertDataFrame(DISTRICT_REPORT_TABLE_NAME, districtsReport)
-  logger.info("Districts report created")
-
-  val populationToNumberOfAccidentsReport = report
-    .generateDataFrameReportForTupleRDD(populationToNumberOfAccidents, POPULATION_TO_ACCIDENTS_REPORT_SCHEMA, reportGenerateDate)
+  NYDataDatabase.insertDataFrame(DISTRICT_REPORT_TABLE_NAME, districtReport)
   NYDataDatabase.insertDataFrame(POPULATION_TO_ACCIDENTS_REPORT_TABLE_NAME, populationToNumberOfAccidentsReport)
-  logger.info("Population to number of accidents report created")
-
-  val accidentCountDuringPhenomenonPerHourReport = report
-    .generateDataFrameReportForTupleRDD(accidentCountDuringPhenomenonPerHour, ACCIDENTS_DURING_PHENOMENON_COUNT_REPORT_SCHEMA, reportGenerateDate)
   NYDataDatabase.insertDataFrame(ACCIDENTS_DURING_PHENOMENON_COUNT_REPORT_TABLE_NAME, accidentCountDuringPhenomenonPerHourReport)
-  logger.info("Accidents count per hour for each phenomenon report created")
+
 }
--- src/main/scala/com/itechart/ny_accidents/report/generators/ReportGenerators.scala
+++ src/main/scala/com/itechart/ny_accidents/report/generators/ReportGenerators.scala
@@ -2,10 +2,13 @@ package com.itechart.ny_accidents.report.generators
 
 import com.google.inject.Inject
 import com.itechart.ny_accidents.constants.GeneralConstants._
+import com.itechart.ny_accidents.constants.ReportsDatabase._
 import com.itechart.ny_accidents.entity.MergedData
 import com.itechart.ny_accidents.report.Reports
 import com.itechart.ny_accidents.service.metric.{DistrictMetricService, PopulationMetricService, TimeMetricService, WeatherMetricService}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Column, DataFrame}
+import org.apache.spark.sql.types.StructType
 
 trait ReportGenerator{
   def apply(data: RDD[MergedData], report: Reports): Seq[Seq[String]] = {
@@ -13,8 +16,14 @@ trait ReportGenerator{
     generateReport(calculated, report)
   }
 
+  def apply(data: RDD[MergedData], report: Reports, databaseCreationDate: Column): DataFrame = {
+    val calculated = calculateReport(data)
+    generateDataFrame(calculated, report, databaseCreationDate)
+  }
+
   protected def calculateReport(data: RDD[MergedData]): RDD[_ <: Product]
   protected def generateReport(data: RDD[_ <: Product], report: Reports): Seq[Seq[String]]
+  protected def generateDataFrame(data: RDD[_ <: Product], report: Reports, creationDate: Column): DataFrame
 }
 
 class DayOfWeekReportGenerator extends ReportGenerator {
@@ -24,6 +33,10 @@ class DayOfWeekReportGenerator extends ReportGenerator {
 
   override protected def generateReport(data: RDD[_ <: Product], report: Reports): Seq[Seq[String]] = {
     report.generateReportForTupleRDD(data, DAY_OF_WEEK_REPORT_HEADER)
+  }
+
+  override protected def generateDataFrame(data: RDD[_ <: Product], report: Reports, creationDate: Column): DataFrame = {
+    report.generateDataFrameReportForTupleRDD(data, DAY_OF_WEEK_REPORT_SCHEMA, creationDate)
   }
 }
 
@@ -35,6 +48,10 @@ class HourOfDayReportGenerator extends ReportGenerator {
   override protected def generateReport(data: RDD[_ <: Product], report: Reports): Seq[Seq[String]] = {
     report.generateReportForTupleRDD(data, HOUR_OF_DAY_REPORT_HEADER)
   }
+
+  override protected def generateDataFrame(data: RDD[_ <: Product], report: Reports, creationDate: Column): DataFrame = {
+    report.generateDataFrameReportForTupleRDD(data, HOUR_OF_DAY_REPORT_SCHEMA, creationDate)
+  }
 }
 
 class PeriodReportGenerator extends ReportGenerator {
@@ -44,6 +61,10 @@ class PeriodReportGenerator extends ReportGenerator {
 
   override protected def generateReport(data: RDD[_ <: Product], report: Reports): Seq[Seq[String]] = {
     report.generateReportForTupleRDD(data, DAY_OF_WEEK_REPORT_HEADER)
+  }
+
+  override protected def generateDataFrame(data: RDD[_ <: Product], report: Reports, creationDate: Column): DataFrame = {
+    report.generateDataFrameReportForTupleRDD(data, DAY_OF_WEEK_REPORT_SCHEMA, creationDate)
   }
 }
 
@@ -55,6 +76,10 @@ class WeatherReportGenerator extends ReportGenerator {
   override protected def generateReport(data: RDD[_ <: Product], report: Reports): Seq[Seq[String]] = {
     report.generateReportForTupleRDD(data, PHENOMENON_REPORT_HEADER)
   }
+
+  override protected def generateDataFrame(data: RDD[_ <: Product], report: Reports, creationDate: Column): DataFrame = {
+    report.generateDataFrameReportForTupleRDD(data, PHENOMENON_REPORT_SCHEMA, creationDate)
+  }
 }
 
 class BoroughReportGenerator extends ReportGenerator {
@@ -64,6 +89,10 @@ class BoroughReportGenerator extends ReportGenerator {
 
   override protected def generateReport(data: RDD[_ <: Product], report: Reports): Seq[Seq[String]] = {
     report.generateReportForTupleRDD(data, BOROUGH_REPORT_HEADER)
+  }
+
+  override protected def generateDataFrame(data: RDD[_ <: Product], report: Reports, creationDate: Column): DataFrame = {
+    report.generateDataFrameReportForTupleRDD(data, BOROUGH_REPORT_SCHEMA, creationDate)
   }
 }
 
@@ -75,6 +104,10 @@ class DistrictReportGenerator extends ReportGenerator {
   override protected def generateReport(data: RDD[_ <: Product], report: Reports): Seq[Seq[String]] = {
     report.generateReportForTupleRDD(data, DISTRICT_REPORT_HEADER)
   }
+
+  override protected def generateDataFrame(data: RDD[_ <: Product], report: Reports, creationDate: Column): DataFrame = {
+    report.generateDataFrameReportForTupleRDD(data, DISTRICT_REPORT_SCHEMA, creationDate)
+  }
 }
 
 class PopulationToNumberOfAccidentsReportGenerator @Inject()(populationService: PopulationMetricService) extends ReportGenerator {
@@ -83,7 +116,11 @@ class PopulationToNumberOfAccidentsReportGenerator @Inject()(populationService: 
   }
 
   override protected def generateReport(data: RDD[_ <: Product], report: Reports): Seq[Seq[String]] = {
-    report.generateReportForTupleRDD(data, DISTRICT_REPORT_HEADER)
+    report.generateReportForTupleRDD(data, POPULATION_TO_ACCIDENTS_REPORT_HEADER)
+  }
+
+  override protected def generateDataFrame(data: RDD[_ <: Product], report: Reports, creationDate: Column): DataFrame = {
+    report.generateDataFrameReportForTupleRDD(data, POPULATION_TO_ACCIDENTS_REPORT_SCHEMA, creationDate)
   }
 }
 
@@ -94,5 +131,9 @@ class AccidentCountDuringPhenomenonPerHourReportGenerator extends ReportGenerato
 
   override protected def calculateReport(data: RDD[MergedData]): RDD[_ <: Product] = {
     WeatherMetricService.calculateAccidentCountDuringPhenomenonPerHour(data)
+  }
+
+  override protected def generateDataFrame(data: RDD[_ <: Product], report: Reports, creationDate: Column): DataFrame = {
+    report.generateDataFrameReportForTupleRDD(data, ACCIDENTS_DURING_PHENOMENON_COUNT_REPORT_SCHEMA, creationDate)
   }
 }
