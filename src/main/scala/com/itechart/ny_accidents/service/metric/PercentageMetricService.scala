1892e586a9d3a24b58ee31fcbe318656e3adf926
Natallia.Melnikovich <Natallia.Melnikovich@itechart-group.com>
2019-05-30 22:03:06 +0300

    #NEAF-83-truncate-double-values-to-2-decimal-places-after-dot

--- src/main/scala/com/itechart/ny_accidents/service/metric/PercentageMetricService.scala
+++ src/main/scala/com/itechart/ny_accidents/service/metric/PercentageMetricService.scala
@@ -6,7 +6,7 @@ class PercentageMetricService {
 
   protected def calculatePercentage[A, B](data: RDD[(B, Iterable[A])], dataLength: Long): RDD[(B, Int, Double)] = {
     val statsMap: RDD[(B, Int)] = data.map(tuple => (tuple._1, tuple._2.size))
-    statsMap.map(tuple => (tuple._1, tuple._2, (((tuple._2.toDouble / dataLength.toDouble) * 10000.0).toInt)/100.0)
+    statsMap.map(tuple => (tuple._1, tuple._2, (((tuple._2.toDouble / dataLength.toDouble) * 10000.0).toInt)/100.0))
   }
 
 }
