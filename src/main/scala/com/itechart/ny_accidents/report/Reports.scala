e1b9c73b3d476cd51c3574362194f0cd00351084
baklanvi <baklanvi@gmail.com>
2019-06-03 17:22:14 +0300

    #NEAF-91 remove unused constant

--- src/main/scala/com/itechart/ny_accidents/report/Reports.scala
+++ src/main/scala/com/itechart/ny_accidents/report/Reports.scala
@@ -1,17 +1,13 @@
 package com.itechart.ny_accidents.report
 
-import java.sql.Date
-
 import com.google.inject.Singleton
 import com.itechart.ny_accidents.spark.Spark
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{Column, DataFrame, Row}
 import org.apache.spark.sql.types.StructType
-import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{Column, DataFrame, Row}
 
 @Singleton
 class Reports {
-  private lazy val PK_DATAFRAME_NAME = "id"
   private lazy val DATE_COLUMN_NAME = "creation_date"
 
   def generateReportForTupleRDD[A <: Product](data: RDD[A], header: Seq[String]): Seq[Seq[String]] = {
