f4533a4a2d30a3589f39ada894420c558ad32254
Vladimir Baklan <34661735+VladimirBaklan@users.noreply.github.com>
2019-06-03 17:34:34 +0300

    Fix parser and mapper (#52)
    * #NEAF-86 fix mapper and parser
    
    * #NEAF-86 remove temporary file

--- src/main/scala/com/itechart/ny_accidents/Application.scala
+++ src/main/scala/com/itechart/ny_accidents/Application.scala
@@ -30,7 +30,7 @@ object Application extends App {
   logger.info("Raw data read")
 
   val mergeData: RDD[MergedData] = mergeService
-    .mergeAccidentsWithWeatherAndDistricts[Accident, MergedData](rawData, mergeService.withoutWeatherMapper).cache()
+    .mergeAccidentsWithWeatherAndDistricts[Accident, MergedData](rawData, mergeService.fullMergeMapper).cache()
 
   logger.info("Merged data size: " + mergeData.count())
 
