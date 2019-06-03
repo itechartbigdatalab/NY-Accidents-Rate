e0a4c3883e1db84acdbd2f9d330c153649cc7cba
baklanvi <baklanvi@gmail.com>
2019-06-03 16:12:25 +0300

    #NEAF-91 recovery year metric app

--- src/main/scala/com/itechart/ny_accidents/YearMetricApp.scala
+++ src/main/scala/com/itechart/ny_accidents/YearMetricApp.scala
@@ -14,30 +14,27 @@ import org.apache.spark.rdd.RDD
 import org.slf4j.LoggerFactory
 
 object YearMetricApp extends App {
-//  lazy val logger = LoggerFactory.getLogger(getClass)
-//
-//  val injector = Guice.createInjector(new GuiceModule)
-//  val accidentsParser = injector.getInstance(classOf[AccidentsParser])
-//  val mergeService = injector.getInstance(classOf[MergeService])
-//  val cacheService = injector.getInstance(classOf[MergedDataCacheDAO])
-//  val report = injector.getInstance(classOf[Reports])
-//  val yearDifferenceService = injector.getInstance(classOf[YearDifferenceMetricService])
-//  sys.addShutdownHook(cacheService.close)
-//
-//  val firstYearRows = accidentsParser.readData(Configuration.FIRST_YEAR_FILE_PATH).cache()
-//  val secondYearRows = accidentsParser.readData(Configuration.SECOND_YEAR_FILE_PATH).cache()
-//  logger.info("Raw data read")
-//
-//  val firstYearMergedData: RDD[MergedData] = mergeService
-//    .mergeAccidentsWithWeatherAndDistricts[Accident, MergedData](firstYearRows, mergeService.withoutWeatherMapper).cache()
-//  logger.info("Merged data size: " + firstYearMergedData.count())
-//  val secondYearMergedData: RDD[MergedData] = mergeService
-//    .mergeAccidentsWithWeatherAndDistricts[Accident, MergedData](secondYearRows, mergeService.withoutWeatherMapper).cache()
-//  logger.info("Merged data size: " + secondYearMergedData.count())
-//
-//  val yearDifference = yearDifferenceService.calculateDifferenceBetweenAccidentCount(firstYearMergedData, secondYearMergedData).cache()
-//  val boroughReport = report.generateReportForTupleRDD[(String, Int)](yearDifference, YEAR_DIFFERENCE_HEADER)
-//  FileWriterUtils.writeToCsv(boroughReport, "reports/year_difference.csv")
-//  logger.info("Year Difference Header report created")
+  lazy val logger = LoggerFactory.getLogger(getClass)
+
+  val injector = Guice.createInjector(new GuiceModule)
+
+  val cacheService = injector.getInstance(classOf[MergedDataCacheDAO])
+  val report = injector.getInstance(classOf[Reports])
+  val yearDifferenceService = injector.getInstance(classOf[YearDifferenceMetricService])
+  sys.addShutdownHook(cacheService.close)
+
+  val firstYearRows = AccidentsParser.readData(Configuration.FIRST_YEAR_FILE_PATH).cache()
+  val secondYearRows = AccidentsParser.readData(Configuration.SECOND_YEAR_FILE_PATH).cache()
+  logger.info("Raw data read")
+
+  val firstYearMergedData: RDD[MergedData] = MergeService.mergeData(firstYearRows).cache()
+  logger.info("Merged data size: " + firstYearMergedData.count())
+  val secondYearMergedData: RDD[MergedData] = MergeService.mergeData(secondYearRows).cache()
+  logger.info("Merged data size: " + secondYearMergedData.count())
+
+  val yearDifference = yearDifferenceService.calculateDifferenceBetweenAccidentCount(firstYearMergedData, secondYearMergedData).cache()
+  val boroughReport = report.generateReportForTupleRDD[(String, Int)](yearDifference, YEAR_DIFFERENCE_HEADER)
+  FileWriterUtils.writeToCsv(boroughReport, "reports/year_difference.csv")
+  logger.info("Year Difference Header report created")
 
 }
